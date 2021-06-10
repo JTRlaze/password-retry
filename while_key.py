bb237dce232fcfda20ac0e74f030cdea8ef325b4
JTRlaze <s107270323@gmail.com>
2021-06-10 17:01:34 +0800

    The new key game

--- while_key.py
+++ while_key.py
@@ -1,10 +1,16 @@
 password = '123qwe'
+i = 3
 x = 1
 while x <= 3:
     user = input('請輸入密碼:')
+
     if user == password:
         print('登入成功')
         break
     else:
-        print('密碼錯誤! 還有{}次機會'.format(3-x))
+        print('密碼錯誤! ')
+        if 3-x > 0:
+            print('還有{}次機會'.format(3-x))
+        else:
+            print('嘗試太多次失敗 請五分鐘後再試')
     x = x + 1
